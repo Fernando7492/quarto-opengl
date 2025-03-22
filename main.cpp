108ea1a1a737d4bda052a5fbf91953cf361eed04
Bruna Ferreira <brunagaby8@gmail.com>
2025-03-22 16:33:28 -0300

    ajuste das prateleiras

--- main.cpp
+++ main.cpp
@@ -480,157 +480,135 @@ void flower(){
     
 }
 
-void wallshelf(){
+void prateleiras(){
 //Prateleiras
       
-      //wall shelf one
+      //prateleira 1
       glColor3f(0.2,0.1,0.1);
       glPushMatrix();
-      glTranslatef(1.5,2.7,3);
-      //glRotatef(22, 0,0,1);
+      glTranslatef(1.5,3.3,3);
       glScalef(0.4, 0.03, 0.2);
       drawCube();
       glPopMatrix();
       
-      //wall shelf two
+      //prateleira 2
       glColor3f(0.2,0.1,0.1);
       glPushMatrix();
-      glTranslatef(1,2.3,3);
-      //glRotatef(22, 0,0,1);
+      glTranslatef(0.8,2.8,3);
       glScalef(0.4, 0.03, 0.2);
       drawCube();
       glPopMatrix();
       
-      //wall shelf three
+      //prateleira 3
       glColor3f(0.2,0.1,0.1);
       glPushMatrix();
-      glTranslatef(0.5,1.9,3);
-      //glRotatef(22, 0,0,1);
+      glTranslatef(0,2.2,3);
       glScalef(0.4, 0.03, 0.2);
       drawCube();
       glPopMatrix();
       
-      //wall shelf four
+      //prateleira 4
       glColor3f(0.2,0.1,0.1);
       glPushMatrix();
-      glTranslatef(1,1.5,3);
-      //glRotatef(22, 0,0,1);
+      glTranslatef(1,1.7,3);
       glScalef(0.4, 0.03, 0.2);
       drawCube();
       glPopMatrix();
       
-      //wall shelf five
+      //prateleira 5
       glColor3f(0.2,0.1,0.1);
       glPushMatrix();
-      glTranslatef(1.5,1.1,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.4, 0.03, 0.2);
+      glTranslatef(1.7,1.1,3);
+      glScalef(0.5, 0.03, 0.2);
       drawCube();
       glPopMatrix();
-      
-      //showpiece on the bottom shelf from left 1
-      glColor3f(0.698, 0.133, 0.133);
-      glPushMatrix();
-      glTranslatef(1.5,1.2,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.04, 0.06, 0.2);
-      drawCube();
-      glPopMatrix();
-      
-      //showpiece on the bottom shelf from left 2
-      glColor3f(0.729, 0.333, 0.827);
-      glPushMatrix();
-      glTranslatef(2,1.2,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.04, 0.06, 0.2);
-      drawCube();
-      glPopMatrix();
-      
-      //showpiece on the bottom shelf from left 3 lower portion
-      glColor3f(0.098, 0.098, 0.439);
-      glPushMatrix();
-      glTranslatef(2.5,1.2,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.04, 0.06, 0.2);
-      drawCube();
-      glPopMatrix();
-      
-      //showpiece on the bottom shelf from left 3 upper portion
-      glColor3f(.529, 0.808, 0.980);
-      glPushMatrix();
-      glTranslatef(2.51,1.35,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.01, 0.05, 0.2);
-      drawCube();
-      glPopMatrix();
-      
-      //showpiece on the top shelf  left 2
-      glColor3f(0.502, 0.502, 0.000);
-      glPushMatrix();
-      glTranslatef(2.5,2.71,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.05, 0.16, 0.01);
-      drawCube();
-      glPopMatrix();
-      
-      //showpiece on the top shelf left 1
+
+    //prateleira 1 - peça 1
       glColor3f(0,0,.9);
       glPushMatrix();
-      glTranslatef(1.8,2.71,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.16, 0.1, 0.01);
+      glTranslatef(1.8,3.31,3.2);
+      glScalef(0.16, 0.1, 0.05);
       drawCube();
       glPopMatrix();
+
+      //prateleira 1 - peça 2
+      glColor3f(0.502, 0.502, 0.000);
+      glPushMatrix();
+      glTranslatef(2.5,3.31,3.2);
+      glScalef(0.05, 0.16, 0.05);
+      drawCube();
+      glPopMatrix();  
       
-      //showpiece on 2nd shelf
+      //prateleira 2 - peça única
       glColor3f(.416, 0.353, 0.804);
       glPushMatrix();
-      glTranslatef(1.3,2.4,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.16, 0.08, 0.01);
+      glTranslatef(1.2,2.9,3.3);
+      glScalef(0.16, 0.09, 0.05);
       drawCube();
       glPopMatrix();
       
-      //showpiece on 3rd shelf left 1
+      //prateleira 3 - peça 1
       glColor3f(0.863, 0.078, 0.235);
       glPushMatrix();
-      glTranslatef(0.4,1.9,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.05, 0.16, 0.01);
+      glTranslatef(0.2,2.21,3.2);
+      glScalef(0.05, 0.16, 0.1);
       drawCube();
       glPopMatrix();
       
-      //showpiece on 3rd shelf left 2
+      //prateleira 3 - peça 2
       glColor3f(0.780, 0.082, 0.522);
       glPushMatrix();
-      glTranslatef(0.7,1.9,3);
-      //glRotatef(22, 0,0,1);
-      glScalef(0.05, 0.12, 0.01);
+      glTranslatef(0.5,2.21,3.2);
+      glScalef(0.05, 0.12, 0.1);
       drawCube();
       glPopMatrix();
       
-      //showpiece on 3rd shelf left 3
+      //prateleira 3 - peça 3
       glColor3f(0.600, 0.196, 0.800);
       glPushMatrix();
-      glTranslatef(1,1.9,3);
-      glScalef(0.05, 0.09, 0.01);
+      glTranslatef(0.8,2.21,3.2);
+      glScalef(0.05, 0.09, 0.1);
       drawCube();
       glPopMatrix();
       
-      //showpiece on 4th shelf
+      //prateleira 4 - peça 2
       glColor3f(0.282, 0.239, 0.545);
       glPushMatrix();
-      glTranslatef(1.8,1.5,3);
+      glTranslatef(1.8,1.71,3);
       glScalef(0.2, 0.1, 0.2);
       drawpyramid();
       glPopMatrix();
       
-      //showpiece on 4th shelf
-      glColor3f(0.251, 0.878, 0.816);
+      //prateleira 4 - peça 1
+      glColor3ub(0,0,139);
       glPushMatrix();
-      glTranslatef(1.4,1.5,3);
+      glTranslatef(1.4,1.71,3);
       glScalef(0.15, 0.1, 0.2);
       drawpyramid();
+      glPopMatrix();
+
+      //prateleira 5 - peça 1
+      glColor3f(0.698, 0.133, 0.133);
+      glPushMatrix();
+      glTranslatef(1.9,1.2,3);
+      glScalef(0.06, 0.06, 0.2);
+      drawCube();
+      glPopMatrix();
+      
+      //prateleira 5 - peça 2
+      glColor3f(0.729, 0.333, 0.827);
+      glPushMatrix();
+      glTranslatef(2.4,1.2,3);
+      glScalef(0.06, 0.06, 0.2);
+      drawCube();
+      glPopMatrix();
+      
+      //prateleira 5 - peça 3
+      glColor3f(0.098, 0.098, 0.439);
+      glPushMatrix();
+      glTranslatef(2.9,1.2,3);
+      glScalef(0.06, 0.06, 0.2);
+      drawCube();
       glPopMatrix();
 
 }
@@ -1188,7 +1166,7 @@ void display(void){
     cupboard();
     lamp();
     flower();
-    wallshelf();
+    prateleiras();
     wardrobe();
     window();
     fan();
